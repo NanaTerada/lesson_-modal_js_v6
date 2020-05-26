d58a5b3a0f78e5a61e6e4bf1e97b4b1be023ddd3
n2 <7worki.nprogress@gmail.com>
2020-05-27 02:11:32 +0900

    #06 要素を取得しよう

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+{
+    const open = document.getElementById('open');
+    const close = document.getElementById('close');
+    const modal = document.getElementById('mask');
+    const mask = document.getElementById('mask');
+}
